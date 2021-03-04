6a64e7186f9126a2291dc58f0c38a3e77bc2929a
VitorLira <vitorlirabraga@gmail.com>
2021-03-04 12:54:57 -0300

    add Desafio 16/17

--- Mundo01/Atividades/Desafio16.py
+++ Mundo01/Atividades/Desafio16.py
@@ -1,1 +1,10 @@
 #Crie um programa que leia um número Real qualquer e mostre na tela a sua porção inteira. ex 6.127 -> 6
+
+from math import trunc
+
+num = float(input('Digite um numero real para saber a sua porção inteira: '))
+print('A porção inteira do número {}, é = {} '.format(num, trunc(num)))
+
+# outro modo de fazer resolver
+
+print('A porção inteira do número {}, é = {} '.format(num, int(num)))
--- Mundo01/Atividades/Desafio17.py
+++ Mundo01/Atividades/Desafio17.py
@@ -1,2 +1,11 @@
 # Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo, calcule e
 # mostre o comprimento da hipotenusa.
+from math import hypot
+co = float(input('Digite o cateto oposto da hipotenusa: '))
+ca = float(input('Digite o cateto adjacente da hipotenusa: '))
+hi = hypot(co, ca)
+print('A hipotenusa do triangulo é = {:.2f}'.format(hi))
+co1 = float(input('Digite o cateto oposto da hipotenusa: '))
+ca1 = float(input('Digite o cateto adjacente da hipotenusa: '))
+hi1 = (co ** 2 + ca ** 2) ** (1/2)
+print('A hipotenusa é igual a {:.2f}'.format(hi1))
